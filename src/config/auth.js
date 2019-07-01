bd6e42cfaf777d1a66cef7ccb850a7bc9d319103
Leandro Stipanich <leandro@ironsystems.com.br>
2019-06-30 21:02:11 -0300

    Primeiro Commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '45f93f30f2e325db7f9ce101e10531b4',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.get('/meetapp', (req, res) => {
+  return res.json({ hello: 'Meetapp' });
+});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+export default routes;
